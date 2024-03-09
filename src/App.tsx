04c9ae38e00e9b3248f7b37b455b9bc30c0fdf05
Max Irendorfer <max.irendorfer@andamp.io>
2024-03-09 14:44:04 +0100

    add drawer for settings

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,20 @@ import { useState } from "react";
 import "./App.css";
 import ReactAudioPlayer from "react-audio-player";
 import Uploader from "./components/Uploader";
-import { Button } from "antd";
+import { Button, Drawer, Input, Select } from "antd";
 import useTranscription from "./hooks/useTranscription";
 import Editor from "./components/Editor";
+import { SettingOutlined } from "@ant-design/icons";
+
+type TransciptionApi = "openAi" | "assemblyAi";
 
 function App() {
   const [audioFile, setAudioFile] = useState<File | null>(null);
   const [transcript, setTranscript] = useState("");
+  const [api, setApi] = useState<TransciptionApi>("openAi");
+  const [settingsOpen, setSettingsOpen] = useState(false);
+  const [apiKey, setApiKey] = useState("");
+
   const { transcribe, messageContext } = useTranscription({
     audioFile,
   });
@@ -37,6 +44,11 @@ function App() {
           marginBottom: 20,
         }}
       >
+        <Button
+          icon={<SettingOutlined />}
+          onClick={() => setSettingsOpen(true)}
+        />
+
         <Uploader onChange={setAudioFile} />
         <Button onClick={handleSubmit} type="primary">
           Transcribe
@@ -50,6 +62,29 @@ function App() {
       />
       <Editor initialValue={transcript} />
       {messageContext}
+      <Drawer
+        title="API Settings"
+        open={settingsOpen}
+        onClose={() => setSettingsOpen(false)}
+        placement="left"
+      >
+        <div style={{ display: "flex", flexDirection: "column", gap: 20 }}>
+          <Select
+            defaultValue={api}
+            onChange={setApi}
+            style={{ width: 120 }}
+            options={[
+              { label: "OpenAI", value: "openAi" },
+              { label: "AssemblyAI", value: "assemblyAi" },
+            ]}
+          ></Select>
+          <Input
+            onChange={(e) => setApiKey(e.target.value)}
+            value={apiKey}
+            placeholder="API Key"
+          />
+        </div>
+      </Drawer>
     </div>
   );
 }
