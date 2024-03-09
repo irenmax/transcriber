de307bfe12fe6f9dcfd2c37b5ad677722ccfc382
Max Irendorfer <max.irendorfer@andamp.io>
2024-03-09 14:51:48 +0100

    add radio to switch between raw and formatted view

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,3 +1,4 @@
+import { Radio } from "antd";
 import TextArea from "antd/es/input/TextArea";
 import { FC, useEffect, useState } from "react";
 import Markdown from "react-markdown";
@@ -8,6 +9,7 @@ type EditorProps = {
 
 const Editor: FC<EditorProps> = ({ initialValue }) => {
   const [text, setText] = useState(initialValue);
+  const [mode, setMode] = useState<"raw" | "formatted">("formatted");
 
   useEffect(() => {
     setText(initialValue);
@@ -21,23 +23,33 @@ const Editor: FC<EditorProps> = ({ initialValue }) => {
     <div
       style={{
         display: "flex",
-        justifyContent: "space-evenly",
-        gap: "20px",
+        flexDirection: "column",
+        gap: 20,
         paddingTop: 50,
         width: "100%",
       }}
     >
-      <div style={{ flex: 1, maxWidth: "30%", paddingTop: "1em" }}>
-        <TextArea
-          autoSize
-          placeholder="Transcription"
-          value={text}
-          onChange={(e) => handleTextEdit(e.target.value)}
-        />
-      </div>
-      <div style={{ flex: 1 }}>
-        <Markdown>{text}</Markdown>
-      </div>
+      <Radio.Group
+        defaultValue={mode}
+        onChange={(e) => setMode(e.target.value)}
+      >
+        <Radio.Button value="raw">Raw</Radio.Button>
+        <Radio.Button value="formatted">Formatted</Radio.Button>
+      </Radio.Group>
+      {mode === "raw" ? (
+        <div style={{ flex: 1, paddingTop: "1em" }}>
+          <TextArea
+            autoSize={{ minRows: 10 }}
+            placeholder="Transcription"
+            value={text}
+            onChange={(e) => handleTextEdit(e.target.value)}
+          />
+        </div>
+      ) : (
+        <div style={{ flex: 1 }}>
+          <Markdown>{text}</Markdown>
+        </div>
+      )}
     </div>
   );
 };
